7772b5c72e09c6ada0578ff637da096b360b59e8
IagoGrelaVarela <igv1010@alu.ubu.es>
2026-06-06 17:06:49 +0200

    Pulido de hundimiento de barcos y solución de errores.

--- UndirLaFlota/Juego/Tablero.cs
+++ UndirLaFlota/Juego/Tablero.cs
@@ -9,7 +9,10 @@ public class Tablero
     private int aciertos = 0;
     private int TotalPuntos;
     public int Dim;
-    
+    // IDs de barco empiezan en 10 para no colisionar con estados (0=agua,1=fallo,3=tocado)
+    private const int baseID = 10;
+    private Dictionary<int, List<(int, int)>> posicionesBarcos;
+    public int UltimoHundidoID { get; private set; }
 
     public Tablero()
     {
@@ -25,15 +28,14 @@ public class Tablero
         {
             TotalPuntos += i;
         }
-        
+
         TableroLimpio();
         GenerarTablero();
     }
 
     public void GenerarTablero()
     {
-        // ID único para cada barco, para detectar cuando se hunden
-        int barcoID = 2;
+        int barcoID = baseID;
         foreach (int i in Barcos)
         {
             GenerarBarco(i, barcoID);
@@ -44,19 +46,19 @@ public class Tablero
     private void GenerarBarco(int tamano, int id)
     {
         Random random = new Random();
-        int x=0; 
-        int y=0; 
-        int dir=0;
-        int P_x=0;
-        int P_y=0;
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        int P_x = 0;
+        int P_y = 0;
         bool entra = false;
         int pos;
         while (!entra)
         {
             entra = true;
-            x = random.Next(0, Dim); 
-            y = random.Next(0, Dim); 
-            dir = random.Next(0, 3);
+            x = random.Next(0, Dim);
+            y = random.Next(0, Dim);
+            dir = random.Next(0, 4);
             P_x = x;
             P_y = y;
             pos = 0;
@@ -93,15 +95,14 @@ public class Tablero
         }
         P_x = x;
         P_y = y;
-        if (entra)
-        {
-            pos = 0;
+        var coords = new List<(int, int)>();
+        pos = 0;
             
-            while (pos<tamano)
+        {
+            TableroList[P_x][P_y] = id;
+            coords.Add((P_x, P_y));
+            switch (dir)
             {
-                TableroList[P_x][P_y] = id;
-                switch (dir)
-                {
                     case 0:
                         P_x++;
                         break;
@@ -114,16 +115,17 @@ public class Tablero
                     case 3:
                         P_y--;
                         break;
-                }
-                
-                pos++;
             }
+            pos++;
         }
+        posicionesBarcos[id] = coords;
     }
 
     private void TableroLimpio()
     {
         aciertos = 0;
+        posicionesBarcos = new Dictionary<int, List<(int, int)>>();
+        TableroList = new List<List<int>>();
         for (int i = 0; i < Dim; i++)
         {
             TableroList.Add(new List<int>());
@@ -134,36 +136,35 @@ public class Tablero
         }
     }
 
+    public List<(int, int)> GetCoordsBarco(int id) => posicionesBarcos[id];
+
     public String Jugada(int x, int y)
     {
-        if (TableroList[x][y] == 0)
+        int celda = TableroList[x][y];
+        if (celda == 0)
         {
             TableroList[x][y] = 1;
             return "Agua";
-        }else if (TableroList[x][y] == 1 || TableroList[x][y] == 3)
+        }
+        else if (celda == 1 || celda == 3)
         {
             return null;
         }
-        else if (TableroList[x][y] == 2)
+        else if (celda >= baseID)
         {
-            int barcoID = TableroList[x][y];
-            aciertos ++;
+            int barcoID = celda;
+            aciertos++;
             TableroList[x][y] = 3;
             if (BarcoHundido(barcoID))
             {
+                UltimoHundidoID = barcoID;
                 if (aciertos >= TotalPuntos)
-            {
-                return "Partida finalizada";
-            }
+                    return "Partida finalizada";
                 else
-                {
                     return "Hundido";
-                }
             }
             return "Tocado";
         }
-
-       
 
         return null;
     }
@@ -171,15 +172,9 @@ public class Tablero
     private bool BarcoHundido(int id)
     {
         for (int i = 0; i < Dim; i++)
-        {
             for (int j = 0; j < Dim; j++)
-            {
                 if (TableroList[i][j] == id)
-                {
                     return false; // Aún no ha sido hundido
-                }
-            }
-        }
         return true;
     }
 }
--- UndirLaFlota/MainPage.xaml.cs
+++ UndirLaFlota/MainPage.xaml.cs
@@ -5,53 +5,46 @@ namespace UndirLaFlota
     public partial class MainPage : ContentPage
     {
         private Tablero tab;
+        private Button[,] botones = new Button[10, 10];
+
         public MainPage()
         {
             tab = new Tablero();
             InitializeComponent();
+            CrearTablero();
+        }
 
+        private void CrearTablero()
+        {
             for (int i = 0; i < tab.Dim; i++)
             {
                 for (int j = 0; j < tab.Dim; j++)
                 {
-                    String Txt = tab.TableroList[i][j].ToString();
-                    if (Txt.Equals("2"))
-                    {
-                        Txt = "0";
-                    }
                     var button = new Button
                     {
-                        Text = Txt,
+                        Text = "0",
                         CommandParameter = new Tuple<int, int>(i, j)
                     };
 
                     button.Clicked += (sender, e) =>
                     {
-
                         var btn = sender as Button;
                         var position = (Tuple<int, int>)btn.CommandParameter;
-                        int row = position.Item1;
-                        int column = position.Item2;
-
-                        // Función que deseas ejecutar
-                        Seleccion(btn, row, column);
+                        Seleccion(btn, position.Item1, position.Item2);
                     };
 
+                    botones[i, j] = button;
                     TableroGrid.Add(button, i + 1, j + 1);
-
-
                 }
             }
         }
 
         public void Seleccion(Button btn, int row, int column)
         {
-            String str = tab.Jugada(row, column);
-            if (str == null)
-            {
+            string str = tab.Jugada(row, column);
+            if (str == null) return;
 
-            }
-            else if (str.Equals("Agua"))
+            if (str.Equals("Agua"))
             {
                 btn.Text = "1";
             }
@@ -61,17 +54,16 @@ namespace UndirLaFlota
             }
             else if (str.Equals("Hundido"))
             {
-                btn.Text = "4";
+                foreach (var (r, c) in tab.GetCoordsBarco(tab.UltimoHundidoID))
+                    botones[r, c].Text = "4";
                 DisplayAlert("Barco", "Barco Hundido!!", "OK");
             }
-
-            
             else if (str.Equals("Partida finalizada"))
             {
-                DisplayAlert("Partidad", "Partida Finalizada!!", "OK");
+                foreach (var (r, c) in tab.GetCoordsBarco(tab.UltimoHundidoID))
+                    botones[r, c].Text = "4";
+                DisplayAlert("Partida", "Partida Finalizada!!", "OK");
             }
         }
-
-
     }
 }
